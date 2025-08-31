27d54f95634527fcdedb2b6dd1ee67c6caf68c22
Mir Misbah Ali <mirmisbahali@hotmail.com>
2025-08-31 22:50:22 +1000

    created pico_bridge package

--- run_ros_container.sh
+++ run_ros_container.sh
@@ -7,8 +7,8 @@ docker run --runtime nvidia -it --rm \
   --user root \
   -v /dev:/dev \
   -v /sys:/sys \
-  -v /home/misbah/Documents/projects/misbah-jetbot/src/jetbot_ws:/ros_ws \
-  -v /home/misbah/Documents/projects/misbah-jetbot/ros_container_configs:/ros_setup \
+  -v ~/projects/misbah-jetbot/src/jetbot_ws:/ros_ws \
+  -v ~/projects/misbah-jetbot/ros_container_configs:/ros_setup \
   --workdir /ros_ws \
   dustynv/ros:humble-ros-base-l4t-r32.7.1 \
   "$@"
